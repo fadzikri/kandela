e76885bac2fc7ae6b58200bd0d2a285c17ff552b
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-04 21:13:56 +0700

    Simpan Keberhasilan

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const senarai = require("./src/senarai/senarai")
+const express = require('express')
+const senarai = require("./src/senarai/senarai.json");
 
-senarai();
+const app = express()
+const port = 3000
+
+app.get('/senarai', (req, res) => {
+  res.json({
+    status: true,
+    result: senarai
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
