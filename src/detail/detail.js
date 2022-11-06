c54aa97f80a36d7e4336afb1d299b46aa98363bc
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-06 08:27:03 +0700

    Get All HTML

---
+++ src/detail/detail.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const pretty = require("pretty");
+const fs = require("fs");
+
+const baseURL = "https://kernelnewbies.org";
+
+const createAllDetailHTML = () => {
+    const senaraiJSON = JSON.parse(fs.readFileSync(`${__dirname}/../senarai/senarai.json`));
+    senaraiJSON.forEach((senarai) => {
+        senarai.links.forEach((link) => {
+            axios.get(`${baseURL}${link}`).then((response) => {
+                let name = link;
+                name = name.replace("/", "");
+
+                fs.writeFileSync(`${__dirname}/${name}.html`, pretty(response.data), "utf-8");
+
+                console.log(`${name}.html berhasil diunduh!`);
+            }).catch((error) => {
+                console.log("gagal");
+            });
+        })
+    })
+}
+
+createAllDetailHTML();
