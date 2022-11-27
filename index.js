07aa33b1f53932d796067845ac220ec038cb4cfe
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-11-27 09:36:52 +0700

    Add semicolon

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const favicon = require('serve-favicon')
+const express = require('express');
+const favicon = require('serve-favicon');
 const senarai = require("./src/senarai/senarai.json");
 const detail = require("./src/detail/detail.json");
 
-const app = express()
+const app = express();
 const port = 3000
 
-app.use(favicon(`${__dirname}/favicon.ico`))
+app.use(favicon(`${__dirname}/favicon.ico`));
 
 app.get('/', (req, res) => {
   res.json({
@@ -14,23 +14,23 @@ app.get('/', (req, res) => {
     message: "Hello...",
     senarai: "https://kandela.cyclic.app/senarai",
     detail: "https://kandela.cyclic.app/detail"
-  })
-})
+  });
+});
 
 app.get('/senarai', (req, res) => {
   res.json({
     status: true,
     result: senarai
-  })
-})
+  });
+});
 
 app.get('/detail', (req, res) => {
   res.json({
     status: true,
     result: detail
-  })
-})
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 })
