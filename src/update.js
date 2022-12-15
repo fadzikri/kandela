e857f559f9d75b38901e54507f5e365180eb23da
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-15 11:48:12 +0700

    FIx

--- src/update.js
+++ src/update.js
@@ -4,4 +4,8 @@ const { createAllDetailHTML, createDetailJSON } = require("./detail/detail");
 setTimeout(() => createSenaraiHTML(), 0);
 setTimeout(() => scrapeSenaraiJSON(), 60000);
 setTimeout(() => createAllDetailHTML(), 120000);
+<<<<<<< HEAD
 setTimeout(() => createDetailJSON(), 240000);
+=======
+setTimeout(() => createDetailJSON(), 180000);
+>>>>>>> 88a57bf (FIx)
