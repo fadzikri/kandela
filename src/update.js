d6ca01c34a127ef619f2899e6f020bb0a9b385c6
fadzikri <109416385+fadzikri@users.noreply.github.com>
2022-12-15 16:47:03 +0700

    thgvyjgvvvvvvvvvvvvvv

--- src/update.js
+++ src/update.js
@@ -2,6 +2,6 @@ const { createSenaraiHTML, scrapeSenaraiJSON } = require("./senarai/senarai");
 const { createAllDetailHTML, createDetailJSON } = require("./detail/detail");
 
 setTimeout(() => createSenaraiHTML(), 0);
-setTimeout(() => scrapeSenaraiJSON(), 30000);
-setTimeout(() => createAllDetailHTML(), 60000);
-setTimeout(() => createDetailJSON(), 120000);
+setTimeout(() => scrapeSenaraiJSON(), 60000);
+setTimeout(() => createAllDetailHTML(), 120000);
+setTimeout(() => createDetailJSON(), 240000);
